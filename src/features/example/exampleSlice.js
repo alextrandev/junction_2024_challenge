af97b6361918c87e2d11e38dd86bbacf3a607dc3
Alex Tran <tranduclong092@gmail.com>
2024-11-09 00:26:52 +0200

    Add extra reducer and test json api

--- src/features/example/exampleSlice.js
+++ src/features/example/exampleSlice.js
@@ -1,7 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios';
+
+const baseApiUrl = "http://localhost:3001/";
+const postsApi = baseApiUrl + "posts";
+
+export const fetchFromApi = createAsyncThunk("example/posts", async () => {
+  const res = await axios.get(postsApi);
+  return res.data;
+});
 
 const initialState = {
   value: 0,
+  posts: [],
 }
 
 export const exampleSlice = createSlice({
@@ -15,8 +25,15 @@ export const exampleSlice = createSlice({
       state.value -= 1
     },
   },
+  extraReducers(builder) {
+    builder
+    // when fetch return the promise, do the action: update products in state with api payload
+    .addCase(fetchFromApi.fulfilled, (state, action) => {
+      state.posts = action.payload;
+    });
+  },
 })
 
-export const { increment, decrement, incrementByAmount } = exampleSlice.actions
+export const { addOne, minusOne } = exampleSlice.actions
 
 export default exampleSlice.reducer
