b455b1d50591c1cdfb3b04c8ac668cab8619628a
Martin Holland <martin.holland@free.fi>
2024-11-08 21:46:13 +0200

    MUI concept suggestion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-function App() {
+import CompanyDataForm from "./components/CompanyDataForm";
 
+function App() {
   return (
     <>
       <p>Hello world! This is React24K</p>
+      <CompanyDataForm />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
