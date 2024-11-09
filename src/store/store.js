18ebb3213eb3f5730483c3848e152ef670717fe4
Alex Tran <tranduclong092@gmail.com>
2024-11-09 19:02:24 +0200

    Add jobSeekerSlice and jobSeekerReducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import companyReducer from "./companySlice";
 import exampleReducer from "./exampleSlice";
 import hashcodeReducer from "./hashcodeSlice";
+import jobSeekerReducer from "./jobSeekerSlice";
 
 export const store = configureStore({
   reducer: {
     example: exampleReducer,
     company: companyReducer,
     hashes: hashcodeReducer,
+    jobSeeker: jobSeekerReducer,
   },
 });
